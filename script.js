f05cf47d80730d5a9ad2f6d9c3f2ff698caa6e7e
gnrrr7 <almknhbwby@gmail.com>
2025-05-28 09:03:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+const questionContainer = document.getElementById("questionContainer");
+const heartLoader = document.getElementById("heartLoader");
+const resultContainer = document.getElementById("resultContainer");
+
+noBtn.addEventListener("mouseover", () => {
+  const newX = Math.floor(Math.random() * questionContainer.offsetWidth);
+  const newY = Math.floor(Math.random() * questionContainer.offsetHeight);
+  noBtn.style.position = "absolute";
+  noBtn.style.left = `${newX}px`;
+  noBtn.style.top = `${newY}px`;
+});
+
+yesBtn.addEventListener("click", () => {
+  heartLoader.style.display = "block";
+  setTimeout(() => {
+    heartLoader.style.display = "none";
+    resultContainer.style.display = "block";
+  }, 3000);
+});
